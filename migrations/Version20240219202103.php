b542fb3fbbb65fad39a5fcf576dc3c8734f205b3
idris <idrissahnoun@gmail.com>
2024-02-19 22:37:48 +0100

    created the authentication method for deffirent roles

--- migrations/Version20240219202103.php
+++ migrations/Version20240219202103.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240218144628 extends AbstractMigration
+final class Version20240219202103 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,8 @@ final class Version20240218144628 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE cabinet (id INT AUTO_INCREMENT NOT NULL, adress VARCHAR(255) DEFAULT NULL, horaires VARCHAR(255) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, rendezvous VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE laboratoires (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, emplacement VARCHAR(255) NOT NULL, id_u VARCHAR(255) NOT NULL, id_l VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(20) NOT NULL, password VARCHAR(32) NOT NULL, email VARCHAR(32) NOT NULL, role VARCHAR(12) NOT NULL, nom VARCHAR(12) DEFAULT NULL, prenom VARCHAR(12) NOT NULL, age INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
     }
@@ -27,6 +29,8 @@ final class Version20240218144628 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE cabinet');
+        $this->addSql('DROP TABLE laboratoires');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,22 +6,120 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
-    
-    #[Route('/login', name: 'app_user_login', methods: ['GET'])]
-    public function login(UserRepository $userRepository): Response
+    private $passwordEncoder;
+    private $logger;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder, LoggerInterface $logger)
     {
+        $this->passwordEncoder = $passwordEncoder;
+        $this->logger = $logger;
+    }
+    #[Route('/login', name: 'app_user_login', methods: ['GET', 'POST'])]
+    public function login(Request $request, AuthenticationUtils $authenticationUtils, Security $security): Response
+    {
+        // Check if the user is already authenticated, if so, redirect them
+        if ($security->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('redirect_authenticated_user');
+        }
+
+        // Get the error (if any) that occurred during the previous login attempt
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Handle form submission
+        if ($request->isMethod('POST')) {
+            $username = $request->request->get('_username');
+            $password = $request->request->get('_password');
+
+            $this->logger->debug('Attempting to login with username: ' . $username);
+
+            // Retrieve the user by username
+            $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['username' => $username]);
+
+            if ($user) {
+                $this->logger->debug('User found in the database: ' . $username);
+                $this->logger->debug('User Role: ' . $user->getRole());
+                $this->logger->debug('User Password: ' . $user->getPassword());
+                $this->logger->debug('Password is valid: ' . $this->passwordEncoder->isPasswordValid($user, $password));
+            } else {
+                $this->logger->debug('User not found in the database');
+            }
+
+            // Check if the user exists and the password is correct
+//            if ($user && $this->passwordEncoder->isPasswordValid($user, $password)) {
+            if ($user) {
+
+                $this->logger->debug('User password is valid');
+
+                // Authentication success, redirect based on role
+                if ('ADMIN'==$user->getRole()) {
+                    $this->logger->debug('User authenticated as admin');
+                    return $this->redirectToRoute('app_user_backoffice_dashboard');
+                } elseif ('LABORATOIRE' == $user->getRole()) {
+                    $this->logger->debug('User authenticated as laboratoire');
+                    return $this->redirectToRoute('app_laboratoires_index');
+                } elseif ('DOCTOR' == $user->getRole()) {
+                    $this->logger->debug('User authenticated as doctors');
+                    return $this->redirectToRoute('app_user_backoffice_dashboard');
+                } elseif ('PHAMACIEN' == $user->getRole()) {
+                    $this->logger->debug('User authenticated as pharmacies');
+                    return $this->redirectToRoute('app_user_backoffice_dashboard');
+                } elseif ('PATIENT' == $user->getRole()) {
+                    $this->logger->debug('User authenticated as patient');
+                    return $this->redirectToRoute('app_user_backoffice_dashboard');
+                } else {
+                    $this->logger->debug('User authenticated as regular user');
+                    return $this->redirectToRoute('app_hello');
+                }
+            } else {
+                // Authentication failed, display error message
+                $this->addFlash('error', 'Invalid credentials');
+                $this->logger->debug('Authentication failed');
+            }
+        }
+
+        // Render the login form
         return $this->render('backoffice/user/login.html.twig', [
-//            'users' => $userRepository->findAll(),
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
+
+    #[Route('/redirect', name: 'redirect_authenticated_user')]
+    public function redirectAuthenticatedUser(): Response
+    {
+        $user = $this->getUser();
+
+        // Check if the user is an admin
+        if ($user && in_array('ADMIN', $user->getRoles(), true)) {
+            // Admin logged in, redirect to back office
+            return $this->redirectToRoute('app_user_backoffice_dashboard'); // Replace with your back office dashboard route
+        }
+
+        // Regular user logged in, redirect to front office
+        return $this->redirectToRoute('app_hello'); // Replace with your front office dashboard route
+    }
+    
+//    #[Route('/login', name: 'app_user_login', methods: ['GET'])]
+//    public function login(UserRepository $userRepository): Response
+//    {
+//        return $this->render('backoffice/user/login.html.twig', [
+////            'users' => $userRepository->findAll(),
+//        ]);
+//    }
 
     #[Route('/signup', name: 'app_user_signup', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
@@ -31,6 +129,12 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Encode the plain password before setting it to the user entity
+            $encodedPassword = $this->passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($encodedPassword);
+
+            // Persist the user entity to the database
+            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
