0ecefc5af8f6bd712d804520cea0e6ac07d4a19d
Med Khalil Ismail <medkhalil.ismail@esprit.tn>
2024-02-22 10:53:00 +0100

    posts entity lastModification attribut done

--- src/Entity/Posts.php
+++ src/Entity/Posts.php
@@ -13,24 +13,28 @@ class Posts
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: "AUTO")]
     #[ORM\Column(type: "integer")]
-    private ?int $id;
+    private int $id;
 
     #[ORM\Column(type: "text")]
     #[Assert\NotBlank(message: 'Le contenu ne peut pas être vide')]
-    #[Assert\Regex(pattern: '/^[a-zA-Z0-9_\s!?.]*$/', message: 'Seules les lettres, les chiffres, les traits de soulignement et les espaces sont autorisés.')]
-    private ?string $contenu;
+    #[Assert\Regex(pattern: '/^[a-zA-Z0-9_\s!?.]*$/', message: 'Seules les lettres, les chiffres,
+     les traits de soulignement et les espaces sont autorisés.')]
+    private string $contenu;
 
     #[ORM\Column(type: "datetime")]
     private \DateTimeInterface $date_creation;
 
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private ?\DateTimeInterface $last_modification;
+
 
     #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(name: "user_id", referencedColumnName: "id")]
-    private ?User $user;
+    private User $user;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank(message: 'Le  ne peut pas être vide')]
-    private ?string $postedAs = null;
+    private string $postedAs = 'Anonyme';
 
     public function getId(): ?int
     {
@@ -48,10 +52,29 @@ class Posts
         return $this;
     }
 
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): void
+    {
+        $this->date_creation = new \DateTime();
+    }
     public function getDateCreation(): ?\DateTimeInterface
     {
         return $this->date_creation;
     }
+
+
+    #[ORM\PreUpdate]
+    public function setModifiedValue(): void
+    {
+        $this->last_modification = new \DateTime();
+    }
+
+    public function getLastModification(): ?\DateTimeInterface
+    {
+        return $this->last_modification;
+    }
+
+
 
     public function setUser(?User $user): self
     {
@@ -62,12 +85,6 @@ class Posts
     public function getUser(): ?User
     {
         return $this->user;
-    }
-
-    #[ORM\PrePersist]
-    public function setCreatedAtValue(): void
-    {
-        $this->date_creation = new \DateTime();
     }
 
     public function getPostedAs(): ?string
